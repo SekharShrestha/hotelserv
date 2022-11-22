8b55b73302951afb41d7ed4f1953dbfafe214422
Sekhar Shrestha <seshrestha17@gmail.com>
2022-11-22 19:25:32 +0530

    Hotelserv first commit with application.yml configs

---
+++ Hotelserv/src/main/java/com/hotelserv/HotelservApplication.java
@@ -0,0 +1,13 @@
+package com.hotelserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelservApplication.class, args);
+	}
+
+}
---
+++ Hotelserv/src/test/java/com/hotelserv/HotelservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
